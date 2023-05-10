973fc2a619115ee257ee98cf0b7185cb372422ee
aah120 <aah120@ic.ac.uk>
2023-05-10 14:41:58 +0100

    remove unused files

--- src/domain/custom-test-result/i-custom-test-result-repo.ts
+++
@@ -1,9 +0,0 @@
-import { CustomTestResult } from '../value-types/custom-test-result';
-import { IDbConnection} from '../services/i-db';
-
-export interface ICustomTestResultRepo {
-  insertOne(
-    customTestResult: CustomTestResult,
-    dbConnection: IDbConnection,
-  ): Promise<string>;
-}
